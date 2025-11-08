dfb87e82f46e9d7d0489859dcf8f50af0efedc85
Anders <anders-10@live.dk>
2025-11-08 23:17:16 +0100

    Feat(terraform): Better terraform setup.

--- {{ cookiecutter.repo_name }}/{% if cookiecutter.use_aws == 'aws' %}infra{% endif %}/{% if cookiecutter.use_aws == 'aws' %}terraform{% endif %}/backend.tf
+++ {{ cookiecutter.repo_name }}/{% if cookiecutter.use_aws == 'aws' %}infra{% endif %}/{% if cookiecutter.use_aws == 'aws' %}terraform{% endif %}/backend.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "AWS_BUCKET_FOR_TF_STATE"
-    key            = "terraform.tfstate"
-    region         = "{{ cookiecutter.aws_region }}"
-    dynamodb_table = "AWS_DYNAMODB_LOCK_TABLE"
-    encrypt        = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
--- {{ cookiecutter.repo_name }}/{% if cookiecutter.use_aws == 'aws' %}infra{% endif %}/{% if cookiecutter.use_aws == 'aws' %}terraform{% endif %}/main.tf
+++ {{ cookiecutter.repo_name }}/{% if cookiecutter.use_aws == 'aws' %}infra{% endif %}/{% if cookiecutter.use_aws == 'aws' %}terraform{% endif %}/main.tf
@@ -1,7 +1,14 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "${var.project_name}-example"
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "myproject-tf-state"
 }
 
-output "example_bucket_name" {
-  value = aws_s3_bucket.example.bucket
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "myproject-tf-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
